024c84f3d99f861798917c833f6b0d9f114cb0dd
vinu2724 <vinayakkhetmalis7@gmail.com>
2024-07-04 17:39:47 +0530

    it the final commit i guess everything is done and working perfectlly

--- Script.js
+++ Script.js
@@ -3,6 +3,7 @@ let music = new Audio("music.mp3")
 let audioTurn = new Audio("ting.mp3")
 let gameover = new Audio("gameover.mp3")
 let turn = "X"
+let isgameover= false;
 
 const changeTurn= () => {
     return turn === "X"?"0":"X"
@@ -11,6 +12,7 @@ const changeTurn= () => {
 
 //Function to check for a win 
 const checkWin =()=>{
+    let boxtext = document.getElementsByClassName('boxtext');
     let wins= [
         [0,1,2],
         [3,4,5],
@@ -22,12 +24,23 @@ const checkWin =()=>{
         [2,4,6]
     ]
  wins.forEach(e=>{
+    if((boxtext[e[0]].innerText === boxtext[e[1]].innerText) && (boxtext[e[2]].innerText === boxtext[e[1]].innerText) && (boxtext[e[0]].innerText !== "")) {
+        document.querySelector('.info').innerText = boxtext[e[0]].innerText + " won"
+        isgameover= true;
+        document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="200px"
      
+        gameover.play()
+        setTimeout(()=>{
+            music.play();
+        },1000)
+
+    }
  })
 }
 
 
 //Game Logic
+
 let boxes = document.getElementsByClassName("box")
 Array.from(boxes).forEach(element=>{
     let boxtext= element.querySelector('.boxtext');
@@ -37,9 +50,29 @@ Array.from(boxes).forEach(element=>{
             turn=changeTurn();
             audioTurn.play();
             checkWin();
+            if(!isgameover){
 
-            document.getElementsByClassName("info")[0].innerText = "turn for" + turn;
+                document.getElementsByClassName("info")[0].innerText = "turn for" + turn;
+            }
         }
     })
 })
+
+
+    // add  onClick listener to reset btn
+    reset.addEventListener('click', ()=> {
+     let boxtexts =document.querySelectorAll('.boxtext');
+     Array.from(boxtexts).forEach(element =>{
+         element.innerText=""
+         checkWin();
+         turn= "X";
+         isgameover=false; 
+
+             document.getElementsByClassName("info")[0].innerText = "turn for" + turn;
+             document.querySelector('.imgbox').getElementsByTagName('img')[0].style.width="0px"
+         
+     })
+     gameover.pause();
+      music.pause();
+    })
 
